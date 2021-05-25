5be026cdac0cb9a682bae7ef2fb426458597a077
Harsh Pareek <32735763+Harsh-Pareek-Commits@users.noreply.github.com>
2021-05-25 09:53:32 +0530

    Add files via upload

---
+++ TripManagementSystem/src/main/java/com/g5/entities/Customer.java
@@ -0,0 +1,91 @@
+package com.g5.entities;
+
+import javax.persistence.Column;
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+@Entity
+@DiscriminatorValue(value = "3")
+public class Customer extends User {
+
+
+	@Column
+	private String customerName;
+	@Column
+	private String address;
+	@Column
+	private String mobileNo;
+	@Column
+	private String email;
+	
+	public Customer() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Customer(String customerName, String address, String mobileNo, String email) {
+		super();
+		this.customerName = customerName;
+		this.address = address;
+		this.mobileNo = mobileNo;
+		this.email = email;
+	}
+	
+	/**
+	 * @return the customerName
+	 */
+	public String getCustomerName() {
+		return customerName;
+	}
+	/**
+	 * @param customerName the customerName to set
+	 */
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+	/**
+	 * @return the address
+	 */
+	public String getAddress() {
+		return address;
+	}
+	/**
+	 * @param address the address to set
+	 */
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	/**
+	 * @return the mobileNo
+	 */
+	public String getMobileNo() {
+		return mobileNo;
+	}
+	/**
+	 * @param mobileNo the mobileNo to set
+	 */
+	public void setMobileNo(String mobileNo) {
+		this.mobileNo = mobileNo;
+	}
+	/**
+	 * @return the email
+	 */
+	public String getEmail() {
+		return email;
+	}
+	/**
+	 * @param email the email to set
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	@Override
+	public String toString() {
+		return "Customer [customerName=" + customerName + ", address=" + address + ", mobileNo=" + mobileNo + ", email="
+				+ email + "]";
+	}
+	
+	
+	
+	
+}
