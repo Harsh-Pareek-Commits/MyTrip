279bc0f768cc5fd1e40821e3496c9fd0486f0449
Harsh Pareek <32735763+Harsh-Pareek-Commits@users.noreply.github.com>
2021-05-25 09:46:59 +0530

    Add files via upload

---
+++ TripManagementSystem/src/main/java/com/g5/entities/Route.java
@@ -0,0 +1,90 @@
+package com.g5.entities;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+public class Route {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private String routeId;
+	@Column
+	private String  routeFrom;
+	@Column
+	private String  routeTo;
+	@OneToMany
+	private  List<Bus> buses;
+	@Temporal(TemporalType.DATE)
+	private  LocalDateTime  departureTime;
+	@Temporal(TemporalType.DATE)
+	private  LocalDateTime   arrivalTime;
+	@Temporal(TemporalType.DATE)
+	private  LocalDate   doj;
+	@Column
+	private String pickupPoint;
+	@Column
+	private  double fare;
+	
+	
+	public Route() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Route(String routeId, String routeFrom, String routeTo, List<Bus> buses, LocalDateTime departureTime,
+			LocalDateTime arrivalTime, LocalDate doj, String pickupPoint, double fare) {
+		super();
+		this.routeId = routeId;
+		this.routeFrom = routeFrom;
+		this.routeTo = routeTo;
+		this.buses = buses;
+		this.departureTime = departureTime;
+		this.arrivalTime = arrivalTime;
+		this.doj = doj;
+		this.pickupPoint = pickupPoint;
+		this.fare = fare;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((routeId == null) ? 0 : routeId.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Route other = (Route) obj;
+		if (routeId == null) {
+			if (other.routeId != null)
+				return false;
+		} else if (!routeId.equals(other.routeId))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "Route [routeId=" + routeId + ", routeFrom=" + routeFrom + ", routeTo=" + routeTo + ", buses=" + buses
+				+ ", departureTime=" + departureTime + ", arrivalTime=" + arrivalTime + ", doj=" + doj
+				+ ", pickupPoint=" + pickupPoint + ", fare=" + fare + "]";
+	}
+	
+	
+
+}
---
+++ TripManagementSystem/src/main/java/com/g5/entities/User.java
@@ -0,0 +1,77 @@
+package com.g5.entities;
+
+import javax.persistence.Column;
+import javax.persistence.DiscriminatorColumn;
+import javax.persistence.DiscriminatorType;
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+
+@Entity
+@Inheritance(strategy=InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "User_Type", discriminatorType = DiscriminatorType.INTEGER)
+@DiscriminatorValue(value = "1")
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int userId;
+	@Column
+	private String userType;
+	@Column
+	private String password;
+	
+	public User() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public User(int userId, String userType, String password) {
+		super();
+		this.userId = userId;
+		this.userType = userType;
+		this.password = password;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((password == null) ? 0 : password.hashCode());
+		result = prime * result + userId;
+		result = prime * result + ((userType == null) ? 0 : userType.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (password == null) {
+			if (other.password != null)
+				return false;
+		} else if (!password.equals(other.password))
+			return false;
+		if (userId != other.userId)
+			return false;
+		if (userType == null) {
+			if (other.userType != null)
+				return false;
+		} else if (!userType.equals(other.userType))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "User [userId=" + userId + ", userType=" + userType + ", password=" + password + "]";
+	}
+	
+
+}
