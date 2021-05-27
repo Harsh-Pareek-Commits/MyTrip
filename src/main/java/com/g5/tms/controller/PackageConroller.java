abfcdc723bcb2835d4980f0009270ba81009338c
Harsh Pareek <32735763+Harsh-Pareek-Commits@users.noreply.github.com>
2021-05-27 10:38:33 +0530

    package delete-mapping

--- src/main/java/com/g5/tms/controller/PackageConroller.java
+++ src/main/java/com/g5/tms/controller/PackageConroller.java
@@ -3,6 +3,7 @@ package com.g5.tms.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,7 +26,7 @@ IPackageService package_service;
 		this.package_service.addPackage(pack);
 		return pack;
 	}
-	@GetMapping("/delete/{packageId}")
+	@DeleteMapping("/delete/{packageId}")
 	public  Package  deletingPackage(@PathVariable int packageId) throws PackageNotFoundException
 	{
 		
