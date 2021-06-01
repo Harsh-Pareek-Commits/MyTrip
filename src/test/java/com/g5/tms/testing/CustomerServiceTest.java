205c383831c741690a463c177848d45ddc624c62
Harsh Pareek <32735763+Harsh-Pareek-Commits@users.noreply.github.com>
2021-06-01 12:53:16 +0530

    TESTING DONE

--- src/test/java/com/g5/tms/testing/CustomerServiceTest.java
+++ src/test/java/com/g5/tms/testing/CustomerServiceTest.java
@@ -6,6 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
 
 import java.lang.reflect.Array;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -18,8 +19,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
+import com.g5.tms.entities.Package;
+import com.g5.tms.entities.PaymentDetails;
+import com.g5.tms.entities.TicketDetails;
+import com.g5.tms.entities.Booking;
+import com.g5.tms.entities.Bus;
 import com.g5.tms.entities.Customer;
+import com.g5.tms.entities.Travels;
 import com.g5.tms.exceptions.CustomerNotFoundException;
 import com.g5.tms.exceptions.PackageNotFoundException;
 import com.g5.tms.repository.ICustomerRepository;
@@ -29,39 +35,34 @@ import com.g5.tms.service.ICustomerService;
 @ActiveProfiles("test")
 
 public class CustomerServiceTest {
-   @Autowired
-   private ICustomerService cust_Service;
-   
-   @Autowired
-   private ICustomerRepository cust_repo;
+	@Autowired
+	private ICustomerService cust_Service;
 
-   @Test
-   public void whenUserIdIsProvided_thenRetrievedNameIsCorrect() throws CustomerNotFoundException {
-      Customer cust =new Customer("HARSH", "XYZ", "12312312312", "xyz@try.com");
-      cust.setUserType("3");
-      cust.setUserId(1);
-      Optional<Customer> opt=Optional.of(cust) ;
-	   Mockito.when(cust_repo.findById(1)).thenReturn(opt);
-      Customer test_cust = cust_Service.viewCustomer(1);
-      assertEquals(test_cust, cust);
-   }
-   @Test
-   public void testCustomerById() throws CustomerNotFoundException 
-   {
-	   Optional<Customer>cust=Optional.empty();
-	   when(cust_repo.findById(1)).thenReturn(cust);
-	   org.junit.jupiter.api.function.Executable executable  = ()-> {
-		   assertNotNull(cust_Service.viewCustomer(1));
-	   };
-	   assertThrows(CustomerNotFoundException.class,executable);
-   }
-   @Test
-   public void testCustomersByPackageId() throws PackageNotFoundException
-   {
-	  // Optional<List<Customer>>cust=Optional.empty();
-	   when(cust_repo.findByPackageId(1)).thenReturn(cust);
-	   org.junit.jupiter.api.function.Executable executable  = ()-> {
-		   assertNotNull(cust_Service.viewCustomer(1));
-	   };
-   }
+	@Autowired
+	private ICustomerRepository cust_repo;
+
+	@Test
+	public void whenUserIdIsProvided_thenRetrievedNameIsCorrect() throws CustomerNotFoundException {
+		Customer cust = new Customer("HARSH", "XYZ", "12312312312", "xyz@try.com");
+		cust.setUserType("3");
+		cust.setUserId(1);
+		Optional<Customer> opt = Optional.of(cust);
+		Mockito.when(cust_repo.findById(1)).thenReturn(opt);
+		Customer test_cust = cust_Service.viewCustomer(1);
+		assertEquals(cust, test_cust);
+	}
+
+	@Test
+	public void testCustomersByPackageId() throws PackageNotFoundException {
+		Customer c1 = new Customer("HARSH", "XYZ", "12312312312", "xyz@try.com");
+		c1.setUserType("3");
+		c1.setUserId(1);
+		Package p1 = new Package(01, "asds", "asdas", "asds", 1000, null, null);
+		LocalDate date = LocalDate.now();
+		Booking b1 = new Booking(100, "ds", "jj", "t", date, p1, 1, null, null);
+		List<Customer> cust = Arrays.asList(c1);
+		Mockito.when(cust_repo.findByPackageId(01)).thenReturn(cust);
+		List<Customer> actual_custlist = cust_Service.viewAllCustomers(01);
+		assertEquals(cust, actual_custlist);
+	};
 }
