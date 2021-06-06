9202089f3f48e55cdee672db4b1b514248ca4fd1
Harsh Pareek <32735763+Harsh-Pareek-Commits@users.noreply.github.com>
2021-06-06 11:01:02 +0530

    REPORT ERROR/SWAGGER/VALIDATION IN DTO DONE

--- src/main/java/com/g5/tms/entities/Report.java
+++ src/main/java/com/g5/tms/entities/Report.java
@@ -4,6 +4,7 @@ import java.util.Set;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -34,7 +35,7 @@ public class Report {
 	private String reportType; 
 	
 	@ApiModelProperty(name = "Bookings", value = "Contails booking details")
-	@ManyToMany
+	@ManyToMany(fetch = FetchType.EAGER)
 	@JoinColumn(name = "bookingId", referencedColumnName = "bookingId")
 	@Valid
 	private Set<Booking> allBookings;
